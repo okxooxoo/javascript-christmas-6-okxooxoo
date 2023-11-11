11120cd78615e32abcee22be488210a55fc49e0a
okxooxoo <okxooxoo@naver.com>
2023-11-12 02:49:34 +0900

    ✨ feat: 이벤트 배지 부여 기능 추가

--- src/Benefit.js
+++ src/Benefit.js
@@ -23,6 +23,17 @@ class Benefit {
     if (orderAmount >= 120000) paymentAmount -= 25000;
     return paymentAmount;
   }
+
+  /**
+   * 이벤트 배지 부여
+   */
+  getEventBadge() {
+    const benefitAmount = this.calculateTotalBenefitAmount();
+    if (benefitAmount >= 20000) return '산타';
+    if (benefitAmount >= 10000) return '트리';
+    if (benefitAmount >= 5000) return '별';
+    return '없음';
+  }
 }
 
 export default Benefit;
