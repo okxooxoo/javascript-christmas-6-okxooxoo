57400beca2c7f077f0e772f37e9dd537ea126c85
okxooxoo <okxooxoo@naver.com>
2023-11-14 21:59:45 +0900

    🚚 move: UI 로직 view 폴더 내부로 이동

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
-import DateValidator from './validator/DateValidator.js';
-import OrderValidator from './validator/OrderValidator.js';
-import { READ } from './constants/message.js';
+import DateValidator from '../validator/DateValidator.js';
+import OrderValidator from '../validator/OrderValidator.js';
+import { READ } from '../constants/message.js';
 
 const InputView = {
   async readDate() {
