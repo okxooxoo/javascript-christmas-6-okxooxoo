5f1c4cb8ccd6caadbc4fd71c23740b6d6a7e2d38
okxooxoo <okxooxoo@naver.com>
2023-11-14 09:34:44 +0900

    ✨ feat: 사용자에게 주문 입력 받는 메소드 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,17 +1,28 @@
 import { Console } from '@woowacourse/mission-utils';
-import { DateValidator } from './validator/DateValidator.js';
+import DateValidator from './validator/DateValidator.js';
+import OrderValidator from './validator/OrderValidator.js';
+import { READ } from './constants/message.js';
 
 const InputView = {
   async readDate() {
     try {
-      const date = await Console.readLineAsync(
-        '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-      );
+      const date = await Console.readLineAsync(READ.date);
       DateValidator.isValidDate(date);
       return parseInt(date);
     } catch (err) {
       Console.print(err.message);
       return this.readDate();
+    }
+  },
+
+  async readOrder() {
+    try {
+      const order = await Console.readLineAsync(READ.order);
+      OrderValidator.isValidOrder(order);
+      return order;
+    } catch (err) {
+      console.print(err.message);
+      return this.readOrder();
     }
   },
 };
