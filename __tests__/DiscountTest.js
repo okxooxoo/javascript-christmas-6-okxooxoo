d074764e87c7bedae936b487e33fe9e72f24b896
okxooxoo <okxooxoo@naver.com>
2023-11-15 19:50:17 +0900

    ✅ test: 사용자 입력값 검증에 대한 테스트 코드 작성

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -2,21 +2,21 @@ import Discount from '../src/domain/Discount.js';
 
 describe('할인 테스트', () => {
   test.each([[1], [2], [8], [9], [15], [16], [22], [23], [29], [30]])(
-    '주말 할인 적용되는지 확인',
+    '주말이면 true을 반환',
     (date) => {
       expect(Discount.isWeekend(date)).toBeTruthy();
     },
   );
 
   test.each([[3], [7], [10], [14], [17], [21], [24], [28], [31]])(
-    '평일 할인 적용되는지 확인',
+    '평일이면 false을 반환',
     (date) => {
       expect(Discount.isWeekend(date)).toBeFalsy();
     },
   );
 
   test.each([[3], [10], [17], [24], [31], [25]])(
-    '특별 할인 적용되는지 확인',
+    '특별 할인 적용되면 true을 반환',
     (date) => {
     expect(Discount.isSpecial(date)).toBeTruthy();
   });
